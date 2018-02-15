c61b2f94547a4a3a00db074e68b57f0f13b2c0af
LoliZarate <36246933+LoliZarate@users.noreply.github.com>
2018-02-14 21:10:29 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,23 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+	
+var numero = Math.floor(Math.random() * 10) + 1;
+
+if( numero == 9 || numero == 10)
+
+{
+    alert("EXCELENTE " + numero);
+}
+else if( numero > 4)
+{
+    alert("APROBÓ " + numero);
+}
+else
+{
+    alert("Vamos, la próxima se puede " + numero);
+}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+var edad;
+var estado;
+
+edad = document.getElementById("edad").value;
+estado = document.getElementById("estadoCivil").value;
+
+if( edad >=18 &&  estado != "soltero")
+{
+    alert("Es soltero y no es menor.")
+}
 
 
 }//FIN DE LA FUNCIÓN
