39d5818880d1d24c53688eac08b5a80bd5d5e87b
LoliZarate <36246933+LoliZarate@users.noreply.github.com>
2018-02-23 20:14:44 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,39 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	//var contador=0;
+	var numero;
+	var mayor;
+	var menor;
+	var numero;
+	var seguir;
+	var flag = 0;
 
-	while(respuesta!='no')
+	do
 	{
-		
-	
-	}
+		numero = parseInt(prompt("Ingrese un numero: "));
+		//contador++;
 
+		//if ( contador == 1)
+		//{
+		//	menor = numero;
+		//	mayor = numero;
+		//}
+		if(numero > mayor || flag == 0) //(contador > mayor)
+		{
+			mayor = numero;
+		}
+		if(numero < menor || flag == 0) //(numero < menor)
+		{
+			menor = numero;
+			flag = 1;
+		}
+
+		seguir = prompt("Desea continuar?: ");
+	}while(seguir == 's');
+
+document.getElementById("maximo").value = "El mayor es " + mayor;
+document.getElementById("minimo").value = "El menor es " + menor;
 
 
 
