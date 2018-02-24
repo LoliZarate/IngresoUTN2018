f155e706828a5fc757e444304e9f2bad2dde6832
LoliZarate <36246933+LoliZarate@users.noreply.github.com>
2018-02-23 22:04:45 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,55 @@
 function Mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var contadorNeg = 0;
+	var contadorPos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	var acumuladorPos = 0;
+	var acumuladorNeg = 0;
+	var diferencia;
+	var seguir;
+	var numero;
+	var promedioPos = 0;
+	var promedioNeg = 0;
 
-	while(respuesta!="no")
+	do
 	{
+		numero = parseInt(prompt("Ingrese un numero: "));
 		
-	
-	}
+		if( numero > 0)
+		{
+			contadorPos++;
+			acumuladorPos = acumuladorPos + numero;
+		}
+		else if( numero < 0)
+		{
+			contadorNeg++;
+			acumuladorNeg = acumuladorNeg + numero;
+		}
+		else
+		{
+			contadorCeros++;
+		}
+		if(numero % 2 == 0)
+		{
+			contadorPares++;
+		}
+
+		seguir = prompt("Desea continuar?: ");	
+	}while(seguir == 's');
+
+if(contadorPos !=0)
+{
+	promedioPos = acumuladorPos / contadorPos;
+}
+if(contadorNeg !=0)
+{
+	promedioNeg = acumuladorNeg / contadorNeg;
+}
+	diferencia = contadorPos - contadorNeg;
+		
+	    document.
 
 
 
